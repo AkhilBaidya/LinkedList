aadd71e262b4a343e76d34b01958748848ff6ef5
U-BSD\463541 <463541@SHS-CYBF6X3.beaverton.k12.or.us>
2023-11-30 10:22:49 -0800

    Stated methods in class according to Canvas criteria

--- Node.h
+++ Node.h
@@ -7,7 +7,19 @@ using namespace std;
 class Node {
 
  public:
+  //constructor:
+  Node(Student* theStudent);
 
+  //destructor:
+  ~Node();
+
+  //get methods:
+  Node* getNext();
+  Student* getStudent();
+
+  //set methods:
+  void setNext(Node* newNext);
+  
  protected:
   //fields (nextnode and student)
   Student* student;
