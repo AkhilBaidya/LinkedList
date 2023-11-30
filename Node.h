34a0cc9a572c3a3fcbaac0779bd6f268fb071c6e
U-BSD\463541 <463541@SHS-CYBF6X3.beaverton.k12.or.us>
2023-11-30 10:20:51 -0800

    Node class has student and next node as fields

--- Node.h
+++ Node.h
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cstring>
+#include "Student.h"
 
 using namespace std;
 
@@ -8,5 +9,7 @@ class Node {
  public:
 
  protected:
-  
+  //fields (nextnode and student)
+  Student* student;
+  Node* nextNode;
 };
