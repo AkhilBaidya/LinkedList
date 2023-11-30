c5145cc30e37364684f958778e7a5322849744a9
U-BSD\463541 <463541@SHS-CYBF6X3.beaverton.k12.or.us>
2023-11-30 10:28:19 -0800

    defined the methods

--- Node.cpp
+++ Node.cpp
@@ -6,15 +6,21 @@ using namespace std;
 
 //constructor:
 Node::Node(Student* theStudent) {
+  student = theStudent;
 }
 //destructor:
 Node::~Node(){
+  delete nextNode;
+  delete student;
 }
 //get methods:
 Node* Node::getNext() {
+  return nextNode;
 }
 Student* Node::getStudent() {
+  return student;
 }
 //set method:
 void Node::setNext(Node* newNext){
+  nextNode = newNext;
 }
