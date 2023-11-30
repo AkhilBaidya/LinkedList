1904e5b15977d81da7526fa96aabbc3a3f0374b7
U-BSD\463541 <463541@SHS-CYBF6X3.beaverton.k12.or.us>
2023-11-30 10:06:47 -0800

    listed destructor, constructor, and class fields

--- Student.h
+++ Student.h
@@ -6,6 +6,16 @@ using namespace std;
 class Student {
 
  public:
+  //constructor:
+  Student(int theId, float theGpa, char theFirstN[20], char theSecN[20]);
 
+  //destructor:
+  ~Student();
+ 
  protected:
+  //fields
+  int id;
+  float gpa;
+  char* firstName;
+  char* secondName;
 };
