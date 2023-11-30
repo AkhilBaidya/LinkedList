3d4c056fd70a294ca280d2975f3c5461727d94e6
U-BSD\463541 <463541@SHS-CYBF6X3.beaverton.k12.or.us>
2023-11-30 10:26:11 -0800

    going to define these Node methods, taken from Node.h

---
+++ Node.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cstring>
+#include "Node.h"
+
+using namespace std;
+
+//constructor:
+Node::Node(Student* theStudent) {
+}
+//destructor:
+Node::~Node(){
+}
+//get methods:
+Node* Node::getNext() {
+}
+Student* Node::getStudent() {
+}
+//set method:
+void Node::setNext(Node* newNext){
+}
