565de59c0bc70c4094ce72af4e057e88710ba1c0
U-BSD\463541 <463541@SHS-CYBF6X3.beaverton.k12.or.us>
2023-11-30 10:11:43 -0800

    defining Student methods

---
+++ Student.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cstring>
+#include "Student.h"
+
+using namespace std;
+
+Student::Student(int theId, float theGpa, char theFirstN[20], char theSecN[20]) {
+
+}
+
+Student::~Student() {
+
+}
+
