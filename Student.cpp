669ecc9581ddbc7ea6130287a0a4da830a0f1fb2
U-BSD\463541 <463541@SHS-CYBF6X3.beaverton.k12.or.us>
2023-11-30 10:13:41 -0800

    defining constructor and destructor

--- Student.cpp
+++ Student.cpp
@@ -4,11 +4,19 @@
 
 using namespace std;
 
-Student::Student(int theId, float theGpa, char theFirstN[20], char theSecN[20]) {
+Student::Student(int theId, float theGpa, char theFirstN[20], char theSecN[20]) { //constructor
+  id = theId;
+  gpa = theGpa;
 
+  firstName = new char[20];
+  strcpy(firstName, theFirstN);
+
+  secondName = new char[20];
+  strcpy(secondName, theSecN);
 }
 
-Student::~Student() {
-
+Student::~Student() { //destructor
+  delete[] firstName;
+  delete[] secondName;
 }
 
