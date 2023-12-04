aa7b8343f97a0fd9182a8e56057332d7839939e2
U-BSD\463541 <463541@SHS-CYBF6X3.beaverton.k12.or.us>
2023-12-04 09:34:43 -0800

    Creating objects to test

--- Main.cpp
+++ Main.cpp
@@ -1,10 +1,28 @@
 #include <iostream>
 #include <cstring>
+#include "Node.h"
 
 using namespace std;
 
 int main(){
 
+  //test constructor:
 
+  //creating students:
+  int id = 100;
+  float gpa = 3.42;
+  char firName1[20] = "David";
+  char secName2[20] = "Gilbert";
+  char firName2[20] = "Sir";
+  char secName2[20] = "Ganondorf";
+  
+  Student* student1 = new Student(id, gpa, firName1, secName1);
+  Student* student2 = new Student(id, gpa, firName2, secName2);
+
+  //creating nodes:
+  Node* node1 = new Node(student1);
+  Node* node2 = new Node(student2);
+
+  
   return 0;
 }
