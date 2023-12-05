a193a483e19cb613bf7d28a664d20bc77b1e537e
Akhil Baidya <akhilbaidya101@gmail.com>
2023-12-04 17:21:57 -0800

    added heading comment

--- Node.cpp
+++ Node.cpp
@@ -1,26 +1,35 @@
+//C++ Programming: Linked List Part 1 - Node Class Definitions
+//by Akhil Baidya
+
+//Submission Date: 12/4/2023
+
+/*This file defines the methods specific to a Node Class object, such as
+the constructor, deconstructor, get methods (for both fields) and the
+set method for establishing the "next node."
+ */
+
 #include <iostream>
 #include <cstring>
 #include "Node.h"
 
 using namespace std;
 
-//constructor:
 Node::Node(Student* theStudent) {
   student = theStudent;
 }
-//destructor:
+
 Node::~Node(){
-  nextNode = NULL; //help from Mr. Galbraith - don't want to delete entire chain of nodes by delete nextNode
+  nextNode = NULL;
   delete student;
 }
-//get methods:
+
 Node* Node::getNext() {
   return nextNode;
 }
 Student* Node::getStudent() {
   return student;
 }
-//set method:
+
 void Node::setNext(Node* newNext){
   nextNode = newNext;
 }
